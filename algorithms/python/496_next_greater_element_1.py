fae0fb8b4773575167f9e19a69bdda84eb46ad8b
Punith Patil <punithp007@gmail.com>
2018-12-15 14:01:39 -0700

    Time: O(n) Space: O(n)

---
+++ algorithms/python/496_next_greater_element_1.py
@@ -0,0 +1,52 @@
+# https://leetcode.com/problems/next-greater-element-i/
+
+# You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+#
+# The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+#
+# Example 1:
+#
+# Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+# Output: [-1,3,-1]
+# Explanation:
+#     For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+#     For number 1 in the first array, the next greater number for it in the second array is 3.
+#     For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+# Example 2:
+#
+# Input: nums1 = [2,4], nums2 = [1,2,3,4].
+# Output: [3,-1]
+# Explanation:
+#     For number 2 in the first array, the next greater number for it in the second array is 3.
+#     For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+# Note:
+#
+# All elements in nums1 and nums2 are unique.
+
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        lookup = {}
+        for i in nums2:
+            if not stack:
+                stack.append(i)
+            elif i <= stack[-1]:
+                stack.append(i)
+            else:
+                while stack and i > stack[-1]:
+                    lookup[stack.pop()] = i
+                stack.append(i)
+        output = []
+
+        for i in nums1:
+            if i in lookup:
+                output.append(lookup[i])
+            else:
+                output.append(-1)
+
+        return output
